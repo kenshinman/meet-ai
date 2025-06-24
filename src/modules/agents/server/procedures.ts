a09056b9adf54d9f2ecff94a7230b6aed03cd187
Kehinde Orilogbon <archkenshin@gmail.com>
2025-06-24 12:18:55 -0400

    15: Update and delete agent

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -2,32 +2,70 @@ import { z } from "zod";
 import { db } from "@/db";
 import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { agentsInsertSchema } from "../schema";
+import { agentsInsertSchema, agentsUpdateSchema } from "../schema";
 import { and, count, desc, eq, getTableColumns, ilike, sql } from "drizzle-orm";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/lib/constants";
 import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
-  getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
-    const [existingAgent] = await db
-      .select({
-        meetingCount: sql<number>`5`, // TODO: add meetingCount column to db
-        ...getTableColumns(agents),
-      })
-      .from(agents)
-      .where(
-        and(
-          eq(agents.id, input.id),
-          eq(agents.userId, ctx.auth.user.id)
+  update: protectedProcedure
+    .input(agentsUpdateSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [updatedAgent] = await db
+        .update(agents)
+        .set(input)
+        .where(
+          and(
+            eq(agents.id, input.id),
+            eq(agents.userId, ctx.auth.user.id)
+          )
+        ).returning();
+
+      if (!updatedAgent) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
+      }
+
+      return updatedAgent;
+    }),
+  remove: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const [removedAgent] = await db
+        .delete(agents)
+        .where(
+          and(
+            eq(agents.id, input.id),
+            eq(agents.userId, ctx.auth.user.id)
+          )
+        ).returning();
+
+      if (!removedAgent) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
+      }
+
+      return removedAgent;
+    }),
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() })).query(async ({ input, ctx }) => {
+      const [existingAgent] = await db
+        .select({
+          meetingCount: sql<number>`5`, // TODO: add meetingCount column to db
+          ...getTableColumns(agents),
+        })
+        .from(agents)
+        .where(
+          and(
+            eq(agents.id, input.id),
+            eq(agents.userId, ctx.auth.user.id)
+          )
         )
-      )
 
-    if (!existingAgent) {
-      throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
-    }
+      if (!existingAgent) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" })
+      }
 
-    return existingAgent;
-  }),
+      return existingAgent;
+    }),
   getMany: protectedProcedure
     .input(z.object({
       page: z.number().default(DEFAULT_PAGE),
--- src/modules/agents/ui/views/agent-id-view.tsx
+++ src/modules/agents/ui/views/agent-id-view.tsx
@@ -3,53 +3,104 @@
 import {ErrorState} from "@/components/error-state";
 import {LoadingState} from "@/components/loading-state";
 import {useTRPC} from "@/trpc/client";
-import {useSuspenseQuery} from "@tanstack/react-query";
+import {
+  useMutation,
+  useQueryClient,
+  useSuspenseQuery,
+} from "@tanstack/react-query";
 import {AgentIdViewHeader} from "../components/agent-id-view-header";
 import {GeneratedAvatar} from "@/components/generated-avatar";
 import {Badge} from "@/components/ui/badge";
 import {VideoIcon} from "lucide-react";
+import {useRouter} from "next/navigation";
+import {toast} from "sonner";
+import {useConfirm} from "@/hooks/use-confirm";
+import {useState} from "react";
+import {UpdateAgentDialog} from "../components/update-agent-dialog";
 
 interface IProps {
   agentId: string;
 }
 
 export const AgentIdView = ({agentId}: IProps) => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
   const trpc = useTRPC();
+
+  const [isUpdateAgentDialogOpen, setIsUpdateAgentDialogOpen] = useState(false);
+
   const {data} = useSuspenseQuery(
     trpc.agents.getOne.queryOptions({id: agentId})
   );
+
+  const {mutate: removeAgent} = useMutation(
+    trpc.agents.remove.mutationOptions({
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(
+          trpc.agents.getMany.queryOptions({})
+        );
+        // TODO: Invalidate free tier usage
+        toast.error("Agent removed successfully!");
+        router.replace("/agents");
+      },
+      onError: (error) => {
+        toast.error(error.message);
+      },
+    })
+  );
+
+  const [ConfirmationDialog, confirm] = useConfirm(
+    "Are you sure?",
+    `The following action will remove ${data.meetingCount} associated settings`
+  );
+
+  const handleRemoveAgent = async () => {
+    const ok = await confirm();
+    if (!ok) return;
+    void removeAgent({id: agentId});
+  };
+
   return (
-    <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
-      <AgentIdViewHeader
-        agentId={agentId}
-        agentName={data.name}
-        onEdit={() => {}}
-        onRemove={() => {}}
+    <>
+      <UpdateAgentDialog
+        initialValues={data}
+        open={isUpdateAgentDialogOpen}
+        onOpenChange={setIsUpdateAgentDialogOpen}
       />
-      <div className="bg-white rounded-lg border">
-        <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
-          <div className="flex items-center gap-x-3">
-            <GeneratedAvatar
-              variant="botttsNeutral"
-              seed={data.name}
-              className="size-10"
-            />
-            <h2 className="text-2xl font-medium">{data.name}</h2>
-          </div>
-          <Badge
-            variant="outline"
-            className="flex items-center gap-x-2 [&>svg]:size-4"
-          >
-            <VideoIcon className="text-blue-700" />
-            {data.meetingCount} {data.meetingCount < 2 ? "meeting" : "meetings"}
-          </Badge>
-          <div className="flex flex-col gap-y-4">
-            <p className="text-lg font-medium">Instructions</p>
-            <p className="text-neutral-800">{data.instructions}</p>
+      <ConfirmationDialog />
+      <div className="flex-1 py-4 px-4 md:px-8 flex flex-col gap-y-4">
+        <AgentIdViewHeader
+          agentId={agentId}
+          agentName={data.name}
+          onEdit={() => setIsUpdateAgentDialogOpen(true)}
+          onRemove={handleRemoveAgent}
+        />
+        <div className="bg-white rounded-lg border">
+          <div className="px-4 py-5 gap-y-5 flex flex-col col-span-5">
+            <div className="flex items-center gap-x-3">
+              <GeneratedAvatar
+                variant="botttsNeutral"
+                seed={data.name}
+                className="size-10"
+              />
+              <h2 className="text-2xl font-medium">{data.name}</h2>
+            </div>
+            <Badge
+              variant="outline"
+              className="flex items-center gap-x-2 [&>svg]:size-4"
+            >
+              <VideoIcon className="text-blue-700" />
+              {data.meetingCount}{" "}
+              {data.meetingCount < 2 ? "meeting" : "meetings"}
+            </Badge>
+            <div className="flex flex-col gap-y-4">
+              <p className="text-lg font-medium">Instructions</p>
+              <p className="text-neutral-800">{data.instructions}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
